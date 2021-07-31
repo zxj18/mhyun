ce2a5559f0f7553eb22822cf292e26359c6ae68c
YuanHsing <tlooo.cute@gmail.com>
2021-08-01 04:19:52 +0800

    Update zywcj.js

--- 海阔视界/zywcj.js
+++ 海阔视界/zywcj.js
@@ -512,7 +512,7 @@ if(flag=='pll'){url='https://vip.gaotian.love/api/?key=sRy0QAq8hqXRlrEtrq&url='+
 if(flag=='languang'){url='https://j.languang.wfss100.com/?url='+url}
 if(flag=='msp'){url='https://titan.mgtv.com.bowang.tv/player/?url='+url}
 if(flag=='kdyx'||flag=='kdsx'){url='http://api.kudian6.com/jm/pdplayer.php?url='+url}
-if(flag=='789pan'){url='https://vip.gaotian.love/api/?key=sRy0QAq8hqXRlrEtrq&url='+url}
+if(flag=='789pan'){url='http://jm.719ns.com:16789/ffjxy/?url='+url}
 if(flag=='fanqie'){url='https://jx.fqzy.cc/jx.php?url='+url}
 if(flag=='mysp'||flag=='xmzy'||flag=='tyun'){url='http://jiexi.sxmj.wang/jx.php?url='+base64Encode(url)}
 if(flag=='lekanzyw'){url='https://bak.ojbkjx.com/?url='+url}
@@ -619,7 +619,7 @@ return play!=""?play:getUrl(pli);
 }else if(src.indexOf("kudian6.com")!=-1){
 var html=request(src);
 return html.match(/url\":.*?[\'\"](.*?)[\'\"]/)[1];
-}else if(src.indexOf("gaotian.love")!=-1){
+}else if(src.indexOf("719ns")!=-1){
 	/*
 var html=request(src);
 eval(getCryptoJS());
