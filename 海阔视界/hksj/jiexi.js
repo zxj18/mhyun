452d80ffd2c016f2e3f0501c3411a60eb64a6d67
YuanHsing <tlooo.cute@gmail.com>
2021-10-06 17:02:34 +0800

    Delete 海阔视界/hksj directory

--- 海阔视界/hksj/jiexi.js
+++
@@ -1,9 +0,0 @@
-全民&&https://jx.quanmingjiexi.com/?url=
-思古&&https://api.sigujx.com/?url=
-黑云&&http://jiexi.380k.com/?url=
-online&&http://bofang.online/?url=
-3JX&&https://api.3jx.top/vip/?url=
-free&&http://joinli.gitee.io/free/index1.html?url=
-618&&http://jx.618g.com/?url=
-醉仙云&&http://api.52sjmz.cn/index.php?url=
-冷月&&https://www.521x5.com/jxjm/?url=
